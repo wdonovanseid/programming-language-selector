44d113d20a7131f2059e533062f54bee308e0459
William Donovan-Seid <wdstwo@live.com>
2020-08-14 14:34:33 -0700

    fix various bugs in extra choices section

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@ function add(num1, num2) {
   return num1 + num2;
 }
 
-
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
     event.preventDefault();
@@ -81,15 +80,15 @@ $(document).ready(function() {
       $("body").addClass("monty").removeClass("main");
     }
 
-    if (rubytotal > csharptotal && rubytotal > pythontotal && rubytotal > gottotal && rubytotal > rusttotal && rubytotal > swifttotal) {
+    if (rubytotal > csharptotal && rubytotal > pythontotal && rubytotal > gototal && rubytotal > rusttotal && rubytotal > swifttotal) {
       $("#pLanguage").text("Ruby on Rails");
       $("#choiceL").text("A's");
       $("#pLangInfo").text("Rails is a development tool which gives web developers a framework, providing structure for all the code they write. The Rails framework helps developers to build websites and applications, because it abstracts and simplifies common repetitive tasks.")
-    } else if (csharptotal > rubytotal && csharptotal > pythontotal && csharptotal > gottotal && csharptotal > rusttotal && csharptotal > swifttotal) {
+    } else if (csharptotal > rubytotal && csharptotal > pythontotal && csharptotal > gototal && csharptotal > rusttotal && csharptotal > swifttotal) {
       $("#pLanguage").text("C#");
       $("#choiceL").text("B's");
       $("#pLangInfo").text("C# is widely used for developing desktop applications, web applications and web services. It is used in creating applications of Microsoft at a large scale. C# is also used in game development in Unity.")
-    } else if (pythontotal > rubytotal && pythontotal > csharptotal && pythontotal > gottotal && pythontotal > rusttotal && pythontotal > swifttotal) {
+    } else if (pythontotal > rubytotal && pythontotal > csharptotal && pythontotal > gototal && pythontotal > rusttotal && pythontotal > swifttotal) {
       $("#pLanguage").text("Python");
       $("#choiceL").text("C's");
       $("#pLangInfo").text("Python is a general-purpose coding language which means that, unlike HTML, CSS, and JavaScript, it can be used for other types of programming and software development besides web development. That includes back end development, software development, data science and writing system scripts among other things.")
@@ -97,15 +96,15 @@ $(document).ready(function() {
       $("#pLanguage").text("Go");
       $("#choiceL").text("D's");
       $("#pLangInfo").text("Go is a statically typed, compiled programming language designed at Google by Robert Griesemer, Rob Pike, and Ken Thompson. Go is syntactically similar to C, but with memory safety, garbage collection, structural typing, and CSP-style concurrency.");
-    } else if (rusttotal > rubytotal && rusttotal > csharptotal && rusttotal > gottotal && rusttotal > pythontotal && pythontotal > swifttotal) {
+    } else if (rusttotal > rubytotal && rusttotal > csharptotal && rusttotal > gototal && rusttotal > pythontotal && rusttotal > swifttotal) {
       $("#pLanguage").text("Rust");
       $("#choiceL").text("E's");
       $("#pLangInfo").text("Rust is a multi-paradigm programming language focused on performance and safety, especially safe concurrency. Rust is syntactically similar to C++, and provides memory safety without using garbage collection. The designers refined the language while writing the Servo layout or browser engine, and the Rust compiler.Rust is a multi-paradigm programming language focused on performance and safety, especially safe concurrency. Rust is syntactically similar to C++, and provides memory safety without using garbage collection. ... The designers refined the language while writing the Servo layout or browser engine, and the Rust compiler.")
-    } else if (swifttotal > rubytotal && swifttotal > csharptotal && swifttotal > gottotal && swifttotal > rusttotal && swifttotal > swifttotal) {
+    } else if (swifttotal > rubytotal && swifttotal > csharptotal && swifttotal > gototal && swifttotal > rusttotal && swifttotal > pythontotal) {
       $("#pLanguage").text("Swift");
       $("#choiceL").text("F's");
       $("#pLangInfo").text("Swift is a general-purpose, multi-paradigm, compiled programming language developed by Apple Inc. for iOS, iPadOS, macOS, watchOS, tvOS, and Linux. Swift is designed to work with Apple's Cocoa and Cocoa Touch frameworks and the large body of existing Objective-C code written for Apple products.");
-    } else if (rubytotal === csharptotal || csharptotal === pythontotal || rubytotal === pythontotal) {
+    } else {
       $("#pLanguage").text("ANY programming language");
       $("#choiceL").text("similar letters");
       $("#pLangInfo").html("<a href='https://coderscat.com/learn-programming-languages'>Click Here!</a>");
