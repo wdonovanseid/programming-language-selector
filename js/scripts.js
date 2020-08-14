0d94435ce5ac644d9378123c79166ddb59d1b97a
William Donovan-Seid <wdstwo@live.com>
2020-08-14 09:18:04 -0700

    add in finished selector 1 as well as id's to all selectors

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   $("form#quiz").submit(function(event) {
     event.preventDefault();
-    const answer1 = $("select#").val();
-    const answer2 = $("select#").val();
-    const answer3 = $("select#").val();
-    const answer4 = $("select#").val();
-    const answer5 = $("select#").val();
+    const answer1 = $("select#ques1").val();
+    const answer2 = $("select#ques2").val();
+    const answer3 = $("select#ques3").val();
+    const answer4 = $("select#ques4").val();
+    const answer5 = $("select#ques5").val();
     
   });
 });
