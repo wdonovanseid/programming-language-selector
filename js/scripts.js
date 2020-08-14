ca79d22251f4a7b2d4d47edf8af1544c47022c95
William Donovan-Seid <wdstwo@live.com>
2020-08-14 14:20:53 -0700

    fix typos to answer in if add statements and filled in templates for extra choices

--- js/scripts.js
+++ js/scripts.js
@@ -40,11 +40,11 @@ $(document).ready(function() {
       csharptotal = add(csharptotal, 1);
     } else if (answer2 === "python") {
       pythontotal = add(pythontotal, 1);
-    } else if (answer1 === "go") {
+    } else if (answer2 === "go") {
       gototal = add(gototal, 1);
-    } else if (answer1 === "rust") {
+    } else if (answer2 === "rust") {
       rusttotal = add(rusttotal, 1);
-    } else if (answer1 === "swift") {
+    } else if (answer2 === "swift") {
       swifttotal = add(swifttotal, 1);
     }
     
@@ -54,11 +54,11 @@ $(document).ready(function() {
       csharptotal = add(csharptotal, 1);
     } else if (answer3 === "python") {
       pythontotal = add(pythontotal, 1);
-    } else if (answer1 === "go") {
+    } else if (answer3 === "go") {
       gototal = add(gototal, 1);
-    } else if (answer1 === "rust") {
+    } else if (answer3 === "rust") {
       rusttotal = add(rusttotal, 1);
-    } else if (answer1 === "swift") {
+    } else if (answer3 === "swift") {
       swifttotal = add(swifttotal, 1);
     }
 
@@ -68,11 +68,11 @@ $(document).ready(function() {
       csharptotal = add(csharptotal, 1);
     } else if (answer4 === "python") {
       pythontotal = add(pythontotal, 1);
-    } else if (answer1 === "go") {
+    } else if (answer4 === "go") {
       gototal = add(gototal, 1);
-    } else if (answer1 === "rust") {
+    } else if (answer4 === "rust") {
       rusttotal = add(rusttotal, 1);
-    } else if (answer1 === "swift") {
+    } else if (answer4 === "swift") {
       swifttotal = add(swifttotal, 1);
     }
 
@@ -93,18 +93,18 @@ $(document).ready(function() {
       $("#pLanguage").text("Python");
       $("#choiceL").text("C's");
       $("#pLangInfo").text("Python is a general-purpose coding language which means that, unlike HTML, CSS, and JavaScript, it can be used for other types of programming and software development besides web development. That includes back end development, software development, data science and writing system scripts among other things.")
-    } else if (pythontotal > rubytotal && pythontotal > csharptotal && pythontotal > gottotal && pythontotal > rusttotal && pythontotal > swifttotal) {
-      $("#pLanguage").text("");
-      $("#choiceL").text("'s");
-      $("#pLangInfo").text("");
-    } else if (pythontotal > rubytotal && pythontotal > csharptotal && pythontotal > gottotal && pythontotal > rusttotal && pythontotal > swifttotal) {
-      $("#pLanguage").text("");
-      $("#choiceL").text("'s");
-      $("#pLangInfo").text("");
-    } else if (pythontotal > rubytotal && pythontotal > csharptotal && pythontotal > gottotal && pythontotal > rusttotal && pythontotal > swifttotal) {
-      $("#pLanguage").text("");
-      $("#choiceL").text("'s");
-      $("#pLangInfo").text("");
+    } else if (gototal > rubytotal && gototal > csharptotal && gototal > pythontotal && gototal > rusttotal && gototal > swifttotal) {
+      $("#pLanguage").text("Go");
+      $("#choiceL").text("D's");
+      $("#pLangInfo").text("Go is a statically typed, compiled programming language designed at Google by Robert Griesemer, Rob Pike, and Ken Thompson. Go is syntactically similar to C, but with memory safety, garbage collection, structural typing, and CSP-style concurrency.");
+    } else if (rusttotal > rubytotal && rusttotal > csharptotal && rusttotal > gottotal && rusttotal > pythontotal && pythontotal > swifttotal) {
+      $("#pLanguage").text("Rust");
+      $("#choiceL").text("E's");
+      $("#pLangInfo").text("Rust is a multi-paradigm programming language focused on performance and safety, especially safe concurrency. Rust is syntactically similar to C++, and provides memory safety without using garbage collection. The designers refined the language while writing the Servo layout or browser engine, and the Rust compiler.Rust is a multi-paradigm programming language focused on performance and safety, especially safe concurrency. Rust is syntactically similar to C++, and provides memory safety without using garbage collection. ... The designers refined the language while writing the Servo layout or browser engine, and the Rust compiler.")
+    } else if (swifttotal > rubytotal && swifttotal > csharptotal && swifttotal > gottotal && swifttotal > rusttotal && swifttotal > swifttotal) {
+      $("#pLanguage").text("Swift");
+      $("#choiceL").text("F's");
+      $("#pLangInfo").text("Swift is a general-purpose, multi-paradigm, compiled programming language developed by Apple Inc. for iOS, iPadOS, macOS, watchOS, tvOS, and Linux. Swift is designed to work with Apple's Cocoa and Cocoa Touch frameworks and the large body of existing Objective-C code written for Apple products.");
     } else if (rubytotal === csharptotal || csharptotal === pythontotal || rubytotal === pythontotal) {
       $("#pLanguage").text("ANY programming language");
       $("#choiceL").text("similar letters");
