c18bf61d003f63a2794b18b745f34327f9f16ef2
William Donovan-Seid <wdstwo@live.com>
2020-08-14 14:44:25 -0700

    fix all variables to be lowerCamelCase

--- js/scripts.js
+++ js/scripts.js
@@ -12,67 +12,67 @@ $(document).ready(function() {
     const answer4 = $("select#ques4").val();
     const answer5 = $("select#ques5").val();
 
-    let rubytotal = 0;
-    let csharptotal = 0;
-    let pythontotal = 0;
-    let gototal = 0;
-    let rusttotal = 0;
-    let swifttotal = 0;
+    let rubyTotal = 0;
+    let csharpTotal = 0;
+    let pythonTotal = 0;
+    let goTotal = 0;
+    let rustTotal = 0;
+    let swiftTotal = 0;
 
     if (answer1 === "ruby") {
-      rubytotal = add(rubytotal, 1);
+      rubyTotal = add(rubyTotal, 1);
     } else if (answer1 === "c#") {
-      csharptotal = add(csharptotal, 1);
+      csharpTotal = add(csharpTotal, 1);
     } else if (answer1 === "python") {
-      pythontotal = add(pythontotal, 1);
+      pythonTotal = add(pythonTotal, 1);
     } else if (answer1 === "go") {
-      gototal = add(gototal, 1);
+      goTotal = add(goTotal, 1);
     } else if (answer1 === "rust") {
-      rusttotal = add(rusttotal, 1);
+      rustTotal = add(rustTotal, 1);
     } else if (answer1 === "swift") {
-      swifttotal = add(swifttotal, 1);
+      swiftTotal = add(swiftTotal, 1);
     }
 
     if (answer2 === "ruby") {
-      rubytotal = add(rubytotal, 1);
+      rubyTotal = add(rubyTotal, 1);
     } else if (answer2 === "c#") {
-      csharptotal = add(csharptotal, 1);
+      csharpTotal = add(csharpTotal, 1);
     } else if (answer2 === "python") {
-      pythontotal = add(pythontotal, 1);
+      pythonTotal = add(pythonTotal, 1);
     } else if (answer2 === "go") {
-      gototal = add(gototal, 1);
+      goTotal = add(goTotal, 1);
     } else if (answer2 === "rust") {
-      rusttotal = add(rusttotal, 1);
+      rustTotal = add(rustTotal, 1);
     } else if (answer2 === "swift") {
-      swifttotal = add(swifttotal, 1);
+      swiftTotal = add(swiftTotal, 1);
     }
     
     if (answer3 === "ruby") {
-      rubytotal = add(rubytotal, 1);
+      rubyTotal = add(rubyTotal, 1);
     } else if (answer3 === "c#") {
-      csharptotal = add(csharptotal, 1);
+      csharpTotal = add(csharpTotal, 1);
     } else if (answer3 === "python") {
-      pythontotal = add(pythontotal, 1);
+      pythonTotal = add(pythonTotal, 1);
     } else if (answer3 === "go") {
-      gototal = add(gototal, 1);
+      goTotal = add(goTotal, 1);
     } else if (answer3 === "rust") {
-      rusttotal = add(rusttotal, 1);
+      rustTotal = add(rustTotal, 1);
     } else if (answer3 === "swift") {
-      swifttotal = add(swifttotal, 1);
+      swiftTotal = add(swiftTotal, 1);
     }
 
     if (answer4 === "ruby") {
-      rubytotal = add(rubytotal, 1);
+      rubyTotal = add(rubyTotal, 1);
     } else if (answer4 === "c#") {
-      csharptotal = add(csharptotal, 1);
+      csharpTotal = add(csharpTotal, 1);
     } else if (answer4 === "python") {
-      pythontotal = add(pythontotal, 1);
+      pythonTotal = add(pythonTotal, 1);
     } else if (answer4 === "go") {
-      gototal = add(gototal, 1);
+      goTotal = add(goTotal, 1);
     } else if (answer4 === "rust") {
-      rusttotal = add(rusttotal, 1);
+      rustTotal = add(rustTotal, 1);
     } else if (answer4 === "swift") {
-      swifttotal = add(swifttotal, 1);
+      swiftTotal = add(swiftTotal, 1);
     }
 
     if (answer5 === "c#") {
@@ -80,27 +80,27 @@ $(document).ready(function() {
       $("body").addClass("monty").removeClass("main");
     }
 
-    if (rubytotal > csharptotal && rubytotal > pythontotal && rubytotal > gototal && rubytotal > rusttotal && rubytotal > swifttotal) {
+    if (rubyTotal > csharpTotal && rubyTotal > pythonTotal && rubyTotal > goTotal && rubyTotal > rustTotal && rubyTotal > swiftTotal) {
       $("#pLanguage").text("Ruby on Rails");
       $("#choiceL").text("A's");
       $("#pLangInfo").text("Rails is a development tool which gives web developers a framework, providing structure for all the code they write. The Rails framework helps developers to build websites and applications, because it abstracts and simplifies common repetitive tasks.")
-    } else if (csharptotal > rubytotal && csharptotal > pythontotal && csharptotal > gototal && csharptotal > rusttotal && csharptotal > swifttotal) {
+    } else if (csharpTotal > rubyTotal && csharpTotal > pythonTotal && csharpTotal > goTotal && csharpTotal > rustTotal && csharpTotal > swiftTotal) {
       $("#pLanguage").text("C#");
       $("#choiceL").text("B's");
       $("#pLangInfo").text("C# is widely used for developing desktop applications, web applications and web services. It is used in creating applications of Microsoft at a large scale. C# is also used in game development in Unity.")
-    } else if (pythontotal > rubytotal && pythontotal > csharptotal && pythontotal > gototal && pythontotal > rusttotal && pythontotal > swifttotal) {
+    } else if (pythonTotal > rubyTotal && pythonTotal > csharpTotal && pythonTotal > goTotal && pythonTotal > rustTotal && pythonTotal > swiftTotal) {
       $("#pLanguage").text("Python");
       $("#choiceL").text("C's");
       $("#pLangInfo").text("Python is a general-purpose coding language which means that, unlike HTML, CSS, and JavaScript, it can be used for other types of programming and software development besides web development. That includes back end development, software development, data science and writing system scripts among other things.")
-    } else if (gototal > rubytotal && gototal > csharptotal && gototal > pythontotal && gototal > rusttotal && gototal > swifttotal) {
+    } else if (goTotal > rubyTotal && goTotal > csharpTotal && goTotal > pythonTotal && goTotal > rustTotal && goTotal > swiftTotal) {
       $("#pLanguage").text("Go");
       $("#choiceL").text("D's");
       $("#pLangInfo").text("Go is a statically typed, compiled programming language designed at Google by Robert Griesemer, Rob Pike, and Ken Thompson. Go is syntactically similar to C, but with memory safety, garbage collection, structural typing, and CSP-style concurrency.");
-    } else if (rusttotal > rubytotal && rusttotal > csharptotal && rusttotal > gototal && rusttotal > pythontotal && rusttotal > swifttotal) {
+    } else if (rustTotal > rubyTotal && rustTotal > csharpTotal && rustTotal > goTotal && rustTotal > pythonTotal && rustTotal > swiftTotal) {
       $("#pLanguage").text("Rust");
       $("#choiceL").text("E's");
       $("#pLangInfo").text("Rust is a multi-paradigm programming language focused on performance and safety, especially safe concurrency. Rust is syntactically similar to C++, and provides memory safety without using garbage collection. The designers refined the language while writing the Servo layout or browser engine, and the Rust compiler.Rust is a multi-paradigm programming language focused on performance and safety, especially safe concurrency. Rust is syntactically similar to C++, and provides memory safety without using garbage collection. ... The designers refined the language while writing the Servo layout or browser engine, and the Rust compiler.")
-    } else if (swifttotal > rubytotal && swifttotal > csharptotal && swifttotal > gototal && swifttotal > rusttotal && swifttotal > pythontotal) {
+    } else if (swiftTotal > rubyTotal && swiftTotal > csharpTotal && swiftTotal > goTotal && swiftTotal > rustTotal && swiftTotal > pythonTotal) {
       $("#pLanguage").text("Swift");
       $("#choiceL").text("F's");
       $("#pLangInfo").text("Swift is a general-purpose, multi-paradigm, compiled programming language developed by Apple Inc. for iOS, iPadOS, macOS, watchOS, tvOS, and Linux. Swift is designed to work with Apple's Cocoa and Cocoa Touch frameworks and the large body of existing Objective-C code written for Apple products.");
