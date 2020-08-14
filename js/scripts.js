a180f2ce86117cd6bcbb8ac6e1499960e9b57a0c
William Donovan-Seid <wdstwo@live.com>
2020-08-14 09:09:14 -0700

    add in submit function to scripts with empty variables

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("form#quiz").submit(function(event) {
+    event.preventDefault();
+    const answer1 = $("select#").val();
+    const answer2 = $("select#").val();
+    const answer3 = $("select#").val();
+    const answer4 = $("select#").val();
+    const answer5 = $("select#").val();
+    
+  });
+});
