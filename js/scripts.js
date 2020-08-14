925076e00378ac8129c6ef14e81bb2da5ca93995
William Donovan-Seid <wdstwo@live.com>
2020-08-14 09:41:31 -0700

    add in function add, and programing language total variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+
 $(document).ready(function() {
   $("form#quiz").submit(function(event) {
     event.preventDefault();
@@ -6,6 +11,11 @@ $(document).ready(function() {
     const answer3 = $("select#ques3").val();
     const answer4 = $("select#ques4").val();
     const answer5 = $("select#ques5").val();
+
+    let rubytotal;
+    let csharptotal;
+    let pythontotal;
+
     
   });
 });
